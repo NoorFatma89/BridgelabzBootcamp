7231a36c83cb311fb429bb3a331cd3b618f7b655
Noor Fatma <nfatma8989@gmail.com>
2023-12-20 18:50:41 +0530

    Checking Employee's Presence

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,21 @@
 package com.bridgelabz.employeewage;
+import java.util.Random;
 
 public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Computation Program!");
+		System.out.println();
+		isEmployeePresentOrAbsent();
+	}
+	public static void isEmployeePresentOrAbsent() {
+		int isAbsent=0;
+		Random random=new Random();
+		int randomNumber=random.nextInt(2);
+		if(randomNumber==isAbsent) {
+			System.out.println("Employee is Absent");
+		}
+		else {
+			System.out.println("Employee is Present");
+		}	
 	}
 }
