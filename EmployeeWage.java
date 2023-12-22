5f84860a3dba2ff0252af2d3b9f341c07118be2e
Noor Fatma <83427246+NoorFatma89@users.noreply.github.com>
2023-12-22 13:13:19 +0530

    Update EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -14,8 +14,10 @@ public class EmployeeWage {
 		System.out.println();
 		switchCase();
 		System.out.println();
+		monthlyWage();
 	}
 	
+//	Calculating weather Employee is Present or Absent
 	public static void isEmployeePresentOrAbsent() {
 		int isAbsent=0;
 		Random random=new Random();
@@ -27,29 +29,40 @@ public class EmployeeWage {
 			System.out.println("Employee is Present");
 		}	
 	}
-
+	
+//	Calculating Employee's Daily Wage
 	public static void dailyEmployeeWage() {
 	    int wagePerHour=20,fullDayHour=8;
-	    System.out.println("Enter Employee's daily Hour : ");
+	    System.out.print("To calculate Wage Enter Full Time Employee's daily Hour : ");
 	    Scanner scanner=new Scanner(System.in);
 		int hour=scanner.nextInt();
 	    if(hour==fullDayHour) {
 		    double employeeWage=wagePerHour*fullDayHour;
 		    System.out.println("It's a Full Time Employee and Daily Employee Wage is : " +employeeWage);
 	    }
+	    else {
+	        System.out.println("Enter correct value");
+	    }
+		scanner.close();
 	}
-	
+
+//	Calculating Part-Time Employee's Daily Wage
 	public static void partTimeEmployeeWage() {
-		int wagePerHour=20, partTimeHour=4; 
-		System.out.println("Enter Employee's daily hour: ");
-		Scanner scanner=new Scanner(System.in);
-		int hour=scanner.nextInt();
-		if(hour==partTimeHour) {
-			double employeeWage=wagePerHour*partTimeHour;
-			System.out.println("It's a Part Time Employee and Daily Employee Wage is : " +employeeWage);
-		}
+	    int wagePerHour=20, partTimeHour=4; 
+	    System.out.print("To calculate Wage Enter Part Time Employee's daily hour: ");
+	    Scanner scanner=new Scanner(System.in);
+            int hour=scanner.nextInt();
+	    if(hour==partTimeHour) {
+		double employeeWage=wagePerHour*partTimeHour;
+		System.out.println("It's a Part Time Employee and Daily Employee Wage is : " +employeeWage);
+	    }
+	    else {
+	    	System.out.println("Enter correct value");
+	    }
+	    scanner.close();
 	}
 	
+//	Solving above problem from Switch Case
 	public static void switchCase() {
 		System.out.println("What do you want to know ?");
 		System.out.println("To check Employee is Present or Absent Press 1: ");
@@ -72,11 +85,13 @@ public class EmployeeWage {
 		default:
 			System.out.println("Incorrect Choice! ");
 		}
+		scanner.close();
 	}
-	
+
+//	Calculating Monthly Wage
 	public static void monthlyWage() {
-		int workingDay=20, wagePerHour=20,fullDayHour=8,partTimeHour=4;
-	    System.out.println("Enter Employee's daily Hour : ");
+	    int workingDay=20, wagePerHour=20,fullDayHour=8,partTimeHour=4;
+	    System.out.print("To calculate Wage Enter Employee's daily Hour : ");
 	    Scanner scanner=new Scanner(System.in);
 		int hour=scanner.nextInt();
 	    if(hour==fullDayHour) {
@@ -86,6 +101,10 @@ public class EmployeeWage {
 	    else if(hour==partTimeHour) {
 			double employeeWage=workingDay*wagePerHour*partTimeHour;
 			System.out.println("It's a Part Time Employee and Daily Employee Wage is : " +employeeWage);
-		}	
+		}
+	    else {
+	    	System.out.println("Enter correct value");
+	    }
+		scanner.close();
 	}
 }
