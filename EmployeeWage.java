bc288a3c30ffa8b46fe700afee45451e198b7b2f
Noor Fatma <83427246+NoorFatma89@users.noreply.github.com>
2023-12-22 13:19:22 +0530

    Update EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -14,9 +14,12 @@ public class EmployeeWage {
 		System.out.println();
 		switchCase();
 		System.out.println();
-		calculatingWageWithCondition();
+		monthlyWage();
+		System.out.println();
+		calculateWageWithCondition();
 	}
 	
+//	Calculating weather Employee is Present or Absent
 	public static void isEmployeePresentOrAbsent() {
 		int isAbsent=0;
 		Random random=new Random();
@@ -28,29 +31,40 @@ public class EmployeeWage {
 			System.out.println("Employee is Present");
 		}	
 	}
-
+	
+//	Calculating Employee's Daily Wage
 	public static void dailyEmployeeWage() {
 	    int wagePerHour=20,fullDayHour=8;
-	    System.out.println("Enter Employee's daily Hour : ");
+	    System.out.print("To calculate Wage Enter Full Time Employee's daily Hour : ");
 	    Scanner scanner=new Scanner(System.in);
 		int hour=scanner.nextInt();
 	    if(hour==fullDayHour) {
 		    double employeeWage=wagePerHour*fullDayHour;
 		    System.out.println("It's a Full Time Employee and Daily Employee Wage is : " +employeeWage);
 	    }
+	    else {
+	        System.out.println("Enter correct value");
+	    }
+		scanner.close();
 	}
-	
+
+//	Calculating Part-Time Employee's Daily Wage
 	public static void partTimeEmployeeWage() {
-		int wagePerHour=20, partTimeHour=4; 
-		System.out.println("Enter Employee's daily hour: ");
-		Scanner scanner=new Scanner(System.in);
-		int hour=scanner.nextInt();
-		if(hour==partTimeHour) {
-			double employeeWage=wagePerHour*partTimeHour;
-			System.out.println("It's a Part Time Employee and Daily Employee Wage is : " +employeeWage);
-		}
+	    int wagePerHour=20, partTimeHour=4; 
+	    System.out.print("To calculate Wage Enter Part Time Employee's daily hour: ");
+	    Scanner scanner=new Scanner(System.in);
+            int hour=scanner.nextInt();
+	    if(hour==partTimeHour) {
+		double employeeWage=wagePerHour*partTimeHour;
+		System.out.println("It's a Part Time Employee and Daily Employee Wage is : " +employeeWage);
+	    }
+	    else {
+	    	System.out.println("Enter correct value");
+	    }
+	    scanner.close();
 	}
 	
+//	Solving above problem from Switch Case
 	public static void switchCase() {
 		System.out.println("What do you want to know ?");
 		System.out.println("To check Employee is Present or Absent Press 1: ");
@@ -73,11 +87,13 @@ public class EmployeeWage {
 		default:
 			System.out.println("Incorrect Choice! ");
 		}
+		scanner.close();
 	}
-	
+
+//	Calculating Monthly Wage
 	public static void monthlyWage() {
-		int workingDay=20, wagePerHour=20,fullDayHour=8,partTimeHour=4;
-	    System.out.println("Enter Employee's daily Hour : ");
+	    int workingDay=20, wagePerHour=20,fullDayHour=8,partTimeHour=4;
+	    System.out.print("To calculate Wage Enter Employee's daily Hour : ");
 	    Scanner scanner=new Scanner(System.in);
 		int hour=scanner.nextInt();
 	    if(hour==fullDayHour) {
@@ -87,23 +103,28 @@ public class EmployeeWage {
 	    else if(hour==partTimeHour) {
 			double employeeWage=workingDay*wagePerHour*partTimeHour;
 			System.out.println("It's a Part Time Employee and Daily Employee Wage is : " +employeeWage);
-		}	
+		}
+	    else {
+	    	System.out.println("Enter correct value");
+	    }
+		scanner.close();
 	}
-	
-	public static void calculatingWageWithCondition() {
+
+//	Calculating Wage with some Condition
+	public static void calculateWageWithCondition() {
 		int wagePerHour=20, totalHours=0,totalDays=0;
 		while(totalHours<100 && totalDays<20) {
-		    System.out.println("Enter Employee's daily Hour : ");
+		    System.out.print("To calculate Wage Enter Employee's daily Hour : ");
 		    Scanner scanner=new Scanner(System.in);
-			int hour=scanner.nextInt();
-			totalHours+=hour;
-			System.out.println("Enter Days : ");
-			int days=scanner.nextInt();
-			totalDays+=days;    
+		    int hour=scanner.nextInt();
+		    totalHours+=hour;
+		    System.out.println("Enter Days : ");
+	            int days=scanner.nextInt();
+		    totalDays+=days;
+		    scanner.close();
 		}
 		double employeeWage=wagePerHour*totalHours*totalDays;
 		System.out.println("Total Working Hour or day is reached for a Month!");
-		System.out.println("Your Wage is :" +employeeWage);
-	    
+		System.out.println("Your Wage is :" +employeeWage);  
 	}
 }
