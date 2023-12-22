89282814eec618c89acf014723b7be9a6f17ba4c
Noor Fatma <nfatma8989@gmail.com>
2023-12-22 09:30:44 +0530

    Refactor Code

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,21 +3,9 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class EmployeeWage {
-	public static void main(String[] args) {
-		System.out.println("Welcome To Employee Wage Computation Program!");
-		System.out.println();
-		isEmployeePresentOrAbsent();
-		System.out.println();
-		dailyEmployeeWage();
-		System.out.println();
-		partTimeEmployeeWage();
-		System.out.println();
-		switchCase();
-		System.out.println();
-		calculatingWageWithCondition();
-	}
 	
-	public static void isEmployeePresentOrAbsent() {
+//	Calculating weather Employee is Present or Absent
+	public void isEmployeePresentOrAbsent() {
 		int isAbsent=0;
 		Random random=new Random();
 		int randomNumber=random.nextInt(2);
@@ -28,30 +16,41 @@ public class EmployeeWage {
 			System.out.println("Employee is Present");
 		}	
 	}
-
-	public static void dailyEmployeeWage() {
+	
+//	Calculating Employee's Daily Wage
+	public void dailyEmployeeWage() {
 	    int wagePerHour=20,fullDayHour=8;
-	    System.out.println("Enter Employee's daily Hour : ");
+	    System.out.print("To calculate Wage Enter Full Time Employee's daily Hour : ");
 	    Scanner scanner=new Scanner(System.in);
 		int hour=scanner.nextInt();
 	    if(hour==fullDayHour) {
 		    double employeeWage=wagePerHour*fullDayHour;
 		    System.out.println("It's a Full Time Employee and Daily Employee Wage is : " +employeeWage);
 	    }
+	    else {
+	    	System.out.println("Enter correct value");
+	    }
+		scanner.close();
 	}
-	
-	public static void partTimeEmployeeWage() {
+
+//	Calculating Part-Time Employee's Daily Wage
+	public void partTimeEmployeeWage() {
 		int wagePerHour=20, partTimeHour=4; 
-		System.out.println("Enter Employee's daily hour: ");
+		System.out.print("To calculate Wage Enter Part Time Employee's daily hour: ");
 		Scanner scanner=new Scanner(System.in);
 		int hour=scanner.nextInt();
 		if(hour==partTimeHour) {
 			double employeeWage=wagePerHour*partTimeHour;
 			System.out.println("It's a Part Time Employee and Daily Employee Wage is : " +employeeWage);
 		}
+		else {
+	    	System.out.println("Enter correct value");
+	    }
+		scanner.close();
 	}
 	
-	public static void switchCase() {
+//	Solving above problem from Switch Case
+	public void switchCase() {
 		System.out.println("What do you want to know ?");
 		System.out.println("To check Employee is Present or Absent Press 1: ");
 		System.out.println("To Calculate Full-Time Employee's Daily Wage Press 2: ");
@@ -73,11 +72,13 @@ public class EmployeeWage {
 		default:
 			System.out.println("Incorrect Choice! ");
 		}
+		scanner.close();
 	}
-	
-	public static void monthlyWage() {
+
+//	Calculating Monthly Wage
+	public void monthlyWage() {
 		int workingDay=20, wagePerHour=20,fullDayHour=8,partTimeHour=4;
-	    System.out.println("Enter Employee's daily Hour : ");
+	    System.out.print("To calculate Wage Enter Employee's daily Hour : ");
 	    Scanner scanner=new Scanner(System.in);
 		int hour=scanner.nextInt();
 	    if(hour==fullDayHour) {
@@ -87,23 +88,29 @@ public class EmployeeWage {
 	    else if(hour==partTimeHour) {
 			double employeeWage=workingDay*wagePerHour*partTimeHour;
 			System.out.println("It's a Part Time Employee and Daily Employee Wage is : " +employeeWage);
-		}	
+		}
+	    else {
+	    	System.out.println("Enter correct value");
+	    }
+		scanner.close();
 	}
-	
-	public static void calculatingWageWithCondition() {
+
+//	Calculating Wage with some Condition
+	public void calculatingWageWithCondition() {
 		int wagePerHour=20, totalHours=0,totalDays=0;
 		while(totalHours<100 && totalDays<20) {
-		    System.out.println("Enter Employee's daily Hour : ");
+		    System.out.print("To calculate Wage Enter Employee's daily Hour : ");
 		    Scanner scanner=new Scanner(System.in);
 			int hour=scanner.nextInt();
 			totalHours+=hour;
 			System.out.println("Enter Days : ");
 			int days=scanner.nextInt();
-			totalDays+=days;    
+			totalDays+=days;
+			scanner.close();
 		}
 		double employeeWage=wagePerHour*totalHours*totalDays;
 		System.out.println("Total Working Hour or day is reached for a Month!");
-		System.out.println("Your Wage is :" +employeeWage);
-	    
+		System.out.println("Your Wage is :" +employeeWage);  
 	}
+	
 }
---
+++ Main.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.employeewage;
+
+public class Main {
+
+		public static void main(String[] args) {
+			System.out.println("Welcome To Employee Wage Computation Program!");
+			System.out.println();
+			EmployeeWage employeeWage=new EmployeeWage();
+			employeeWage.isEmployeePresentOrAbsent();
+			System.out.println();
+			employeeWage.dailyEmployeeWage();
+			System.out.println();
+			employeeWage.partTimeEmployeeWage();
+			System.out.println();
+			employeeWage.switchCase();
+			System.out.println();
+			employeeWage.calculatingWageWithCondition();
+		}
+
+}
